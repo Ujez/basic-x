1ef47847fff7a178f77b4304ec8c9ba2501c7237
ujez <ujezujez@gmail.com>
2022-07-26 04:24:57 +0100

    inserted data with the use of the Query Builder

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Redirect;
@@ -26,16 +27,22 @@ class CategoryController extends Controller
             ]);
 
 
-        Category::insert([
-            'category_name' => $request->category_name,
-            'user_id' => Auth::user()->id,
-            'created_at' => Carbon::now(),
-        ]);
+        // Category::insert([
+        //     'category_name' => $request->category_name,
+        //     'user_id' => Auth::user()->id,
+        //     'created_at' => Carbon::now(),
+        // ]);
 
         // $category = new Category;
         // $category ->category_name = $request->category_name;
         // $category -> user_id = Auth::user()->id;
         // $category->save();
+
+        //Using the Query builder pattern to insert data
+        $data = array();
+        $data['category_name'] = $request->category_name;
+        $data['user_id'] = Auth::user()->id;
+        DB::table('categories')->insert($data);
 
         return Redirect()->back()->with('success', 'Category Inserted Successfully');
 
